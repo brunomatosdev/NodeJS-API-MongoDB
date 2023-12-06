1fa702f154bc7fe5146121824254aa6dafd0716d
BrunoMatosDev <sotamenterprise@gmail.com>
2023-12-06 21:39:23 +0000

    Server Done and Running With Multiple CRUD  Functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,59 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+const Film = mongoose.model("Film", {
+  title: String,
+  description: String,
+  image_url: String,
+  trailer_url: String,
+});
+
+app.get("/", async (req, res) => {
+  const films = await Film.find();
+  return res.send(films);
+});
+
+app.delete("/:id", async (req, res) => {
+  const film = await Film.findOneAndDelete(req.params.id);
+  return res.send(film);
+});
+
+app.put("/:id", async (req, res) => {
+  const film = await Film.findByIdAndUpdate(
+    req.params.id,
+    {
+      title: req.body.title,
+      description: req.body.description,
+      image_url: req.body.image_url,
+      trailer_url: req.body.trailer_url,
+    },
+    {
+      new: true,
+    }
+  );
+
+  return res.send(film);
+});
+
+app.post("/", async (req, res) => {
+  const film = new Film({
+    title: req.body.title,
+    description: req.body.description,
+    image_url: req.body.image_url,
+    trailer_url: req.body.trailer_url,
+  });
+
+  await film.save();
+  return res.send(film);
 });
 
 app.listen(port, () => {
+  mongoose.connect(
+    "mongodb+srv://Admin:12345@nodejs-api.axjulnd.mongodb.net/?retryWrites=true&w=majority"
+  );
   console.log(`Listening On Port: ${port}`);
 });
